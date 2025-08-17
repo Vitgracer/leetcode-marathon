8a5bda922b892bc11f181a3ac25625b00cd164dd
Vitaly <vsg.sam@gmail.com>
2025-08-17 15:32:12 +0200

    88 passed

---
+++ python/88_merge_sorted_array_easy.py
@@ -0,0 +1,35 @@
+"""
+https://leetcode.com/problems/merge-sorted-array/
+"""
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        m_cur = m - 1
+        n_cur = n - 1
+
+        for i in range(len(nums1) - 1, -1, -1):
+            if m_cur == -1:
+                nums1[i] = nums2[n_cur]
+                n_cur -=1
+                continue
+
+            if n_cur == -1:
+                nums1[i] = nums1[m_cur]
+                m_cur -=1
+                continue
+            
+            if nums1[m_cur] > nums2[n_cur]:
+                nums1[i] = nums1[m_cur]
+                m_cur -= 1
+            else:
+                nums1[i] = nums2[n_cur]
+                n_cur -= 1        
+        
+        return 
+    
+solution = Solution()
+nums1 = [0]
+m = 0
+nums2 = [1]
+n = 1
+solution.merge(nums1, m, nums2, n)
