0d9504e34b392a6b924c566e277e44f20c343e91
Vitaly <vsg.sam@gmail.com>
2025-08-19 15:34:23 +0200

    448 more elegant

--- python/448_find_all_numbers_disappeared_in_an_array_easy.py
+++ python/448_find_all_numbers_disappeared_in_an_array_easy.py
@@ -2,8 +2,18 @@
 https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 """
 
+# dummy solution
+# class Solution(object):
+#     def findDisappearedNumbers(self, nums):
+#         return list(set(range(1, len(nums) + 1)) - set(nums))
 
+# elegant solution with o(1) in memory
 class Solution(object):
     def findDisappearedNumbers(self, nums):
-        return list(set(range(1, len(nums) + 1)) - set(nums))
-        
+        for i, num in enumerate(nums):
+            index = abs(num) - 1
+
+            if nums[index] > 0:
+                nums[index] *= -1
+
+        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
