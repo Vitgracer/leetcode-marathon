335c14b4536933a41423229bbdff983fd57e9a92
Vitaly <vsg.sam@gmail.com>
2025-09-01 21:52:43 +0200

    747 beats 100% complexity, o(n)

---
+++ python/747_largest_number_at_least_twice_of_others_easy.py
@@ -0,0 +1,30 @@
+"""
+https://leetcode.com/problems/largest-number-at-least-twice-of-others/description/
+"""
+
+# simpliest solution: sort and compare max and before-max element. o(n*log(n))
+class Solution(object):
+    def dominantIndex(self, nums):
+        max_el, max_ind = 0, -1
+        sec_max_el = 0
+
+        for i, el in enumerate(nums):
+            if el >= max_el:
+                prev_max = max_el
+                max_el = el
+                max_ind = i
+                sec_max_el = prev_max
+            else:
+                if el > sec_max_el:
+                    sec_max_el = el
+        
+        if sec_max_el == 0:
+            sec_max_el = 1e-8
+        
+        if max_el / sec_max_el >= 2:
+            return max_ind
+        else:
+            return -1
+
+sol = Solution()
+sol.dominantIndex([[0,0,0,1]])
