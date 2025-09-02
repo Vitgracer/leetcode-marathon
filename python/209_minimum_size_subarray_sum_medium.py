19c633a5a2f9814d813cee27eef0690557d67db3
Vitaly <vsg.sam@gmail.com>
2025-09-02 22:33:20 +0200

    209 passed

--- python/209_minimum_size_subarray_sum_medium.py
+++ python/209_minimum_size_subarray_sum_medium.py
@@ -6,20 +6,20 @@ class Solution(object):
     def minSubArrayLen(self, target, nums):
         l = 0
         r = 0
-        min_len = 0
+        min_len = float("inf") 
         sum = nums[0]
 
-        while l!= len(nums) - 1:
-            if sum < target and r < len(nums) - 1:
-                r += 1
-                sum += nums[r]
+        while True:
             if sum >= target:
-                min_len = r - l + 1
+                min_len = min(min_len, r - l + 1)
                 sum -= nums[l]
                 l += 1
-            if r == len(nums) - 1 and sum < target:
-                return min_len
-        return min_len
+            elif r < len(nums) - 1:
+                r += 1
+                sum += nums[r]
+            else:
+                break
+        return 0 if min_len == float("inf") else min_len
 
 sol = Solution()
 sol.minSubArrayLen(5, [5])
