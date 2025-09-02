1ea3818ff201934f664bbaf246664125bc6bb625
Vitaly <vsg.sam@gmail.com>
2025-09-02 19:48:44 +0200

    66 passed 100%

---
+++ python/66_plus_one_easy.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/plus-one/description/
+"""
+
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        digits = [1] + digits
+        return digits
+        
