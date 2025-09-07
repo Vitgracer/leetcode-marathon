397a85c670c8c2dca1cfa72f8489726f42b24f82
Vitaly <vsg.sam@gmail.com>
2025-09-07 21:31:32 +0200

    1 passed

---
+++ python/1_two_sum_easy.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/two-sum/description/
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+        for i, val in enumerate(nums):
+            if val in d:
+                return d[val], i
+            else:
+                d[target - val] = i
+
+sol = Solution()
+sol.twoSum([2,7,11,15], 9)
+        
